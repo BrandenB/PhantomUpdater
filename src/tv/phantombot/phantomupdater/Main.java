8dfcb06bc8d1c7e2010245099ef7b9ef67285ed8
BrandenB <18152900+BrandenB@users.noreply.github.com>
2018-07-28 23:56:47 -0300

    Finished updaters - still need to test on Linux and macOS

--- src/tv/phantombot/phantomupdater/Main.java
+++ src/tv/phantombot/phantomupdater/Main.java
@@ -5,16 +5,14 @@
  */
 package tv.phantombot.phantomupdater;
 
-import tv.phantombot.phantomupdater.util.CommandExecutor;
-import tv.phantombot.phantomupdater.util.LaunchTypes.Type;
 
 /**
  *
  * @author ScaniaTV
  */
 public final class Main {
-    // Type of way to launch the bot.
-    private static Type launchType;
+    // Version to download.
+    private static String version = null;
     // Location where to store the backup.
     private static String backupLocation = null;
     
@@ -30,62 +28,23 @@ public final class Main {
      * 
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         if (args.length > 0) {
-            // Get the launch type.
-            // This is used once the update is done and we launch the bot again.
-            switch (args[0]) {
-                case "service":
-                    launchType = Type.SERVICE;
-                    break;
-                case "batch":
-                    launchType = Type.BATCH;
-                    break;
-                case "manual":
-                    launchType = Type.MANUAL;
-                    break;
-                default:
-                    // Return main without any arguments.
-                    main(new String[0]);
-            }
-                
+            // Get the version.
+            version = args[0];
+            
             // Get the backup location, if any.
             if (args.length > 1) {
                 backupLocation = args[1];
             }
             
             // Start the update.
-            PhantomUpdater.update(backupLocation);
-            
-            // Our command exec.
-            CommandExecutor ce = null;
-            // Start PhantomBot again.
-            switch (launchType) {
-                case BATCH:
-                    ce = new CommandExecutor("cmd /c start launch.bat");
-                    break;
-                case SHELL:
-                    ce = new CommandExecutor("sudo ./launch.sh");
-                    break;
-                case SERVICE:
-                    ce = new CommandExecutor("sudo systemctl start phantombot");
-                    break;
-            }
-            
-            // This should only null if it's a manual update.
-            if (ce != null) {
-                // Run the command.
-                if (ce.exec()) {
-                    System.out.println("Launching PhantomBot!");
-                } else {
-                    System.out.println("Failed to launch PhantomBot...");
-                }
-            }
+            PhantomUpdater.update(version, backupLocation);
         } else {
             // Print error.
-            System.err.println("Failed to get launch type.");
+            System.err.println("Failed to update due to missing params.");
         }
         // Shutdown the updater.
-        System.exit(0);
+        //System.exit(0);
     }
 }
--- src/tv/phantombot/phantomupdater/PhantomUpdater.java
+++ src/tv/phantombot/phantomupdater/PhantomUpdater.java
@@ -10,8 +10,11 @@ import tv.phantombot.phantomupdater.api.GitHubAPI;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
 
 import org.apache.commons.io.FileUtils;
+
 import org.zeroturnaround.zip.ZipUtil;
 
 /**
@@ -21,10 +24,20 @@ import org.zeroturnaround.zip.ZipUtil;
 public final class PhantomUpdater {
     // File name to save the zip as.
     private static final String DOWNLOAD_FILE_NAME = "PhantomBot.zip";
+    // Nightly download URL.
+    private static final String NIGHTLY_DOWNLOAD_URL = "https://github.com/PhantomBot/nightly-build/blob/master/PhantomBot-nightly.zip?raw=true";
     // Location of where to save the file.
     private static final String DOWNLOAD_LOCATION = "./";
     // Backup folder prefix.
     private static final String BACKUP_FILE_PREFIX = "PhantomBot_Backup_";
+    // New files downloaded.
+    private static final ArrayList<String> NEW_FILES = new ArrayList<>();
+    // Max fails before quit.
+    private static final int MAX_FAILS = 10;
+    // Failed updates.
+    private static int fails = 0;
+    // Last fail time.
+    private static long lastFail = 0l;
     
     /**
      * Class constructor.
@@ -34,26 +47,63 @@ public final class PhantomUpdater {
     }
     
     /**
+     * Method that checks how many times we failed to update.
+     * 
+     * @return 
+     */
+    private static boolean failCheck() {
+        if (fails > MAX_FAILS) {
+            return false;
+        } else {
+            fails++;
+            
+            if (System.currentTimeMillis() > (lastFail + 2000)) {
+                try {
+                    Thread.sleep((long)2000);
+                } catch (InterruptedException ex) {
+                    // Ignore.
+                    //ex.printStackTrace();
+                }
+            }
+            
+            lastFail = System.currentTimeMillis();
+        }
+        
+        return true;
+    }
+    
+    /**
      * Method that is used to start the bot update.
      * 
+     * @param version Nightly or Stable.
      * @param backupLocation Location to backup the file to.
      */
-    public static void update(final String backupLocation) {
+    public static void update(final String version, final String backupLocation) {
         System.out.println("Fetching latest version of PhantomBot...");
+        String downloadUrl;
+        
         // Get the download URL.
-        String downloadUrl = GitHubAPI.getLatestDownloadUrl();
+        if (version.equalsIgnoreCase("nightly")) {
+            downloadUrl = NIGHTLY_DOWNLOAD_URL;
+        } else {
+            downloadUrl = GitHubAPI.getLatestDownloadUrl();
+        }
             
         // If the URL is null, try again.
         if (downloadUrl == null) {
-            update(backupLocation);
+            if (failCheck()) {
+                update(version, backupLocation);
+            } else {
+                System.out.println("Failed to update due to max error attempts.");
+            }
+            return;
         }
                 
         // Try to backup, if it fails, don't update.
         if (backupFiles(".", backupLocation)) {
             // If downloaded, extract the files.
             if (downloadFile(downloadUrl)) {
-                System.out.println("Starting update...");
-                System.out.println();
+                System.out.println("Starting update...\n");
                 // Unpack the zip.
                 unpackDownload(DOWNLOAD_LOCATION + DOWNLOAD_FILE_NAME, DOWNLOAD_LOCATION);
                 // Print the success message.
@@ -93,6 +143,41 @@ public final class PhantomUpdater {
     }
     
     /**
+     * Method that makes sure that we are allowed to remove the file.
+     * 
+     * @param file
+     * @return 
+     */
+    private static boolean canRemoveFile(File file) {
+        String fileName = file.getName();
+        String filePath = file.getPath();
+        
+        return !fileName.endsWith(".txt") && !fileName.contains(".db") &&
+               !filePath.contains("config") && !filePath.contains("addons") &&
+               !filePath.contains("custom") && !filePath.contains(".zip");
+    }
+    
+    /**
+     * Method that removes old files that no longer exist.
+     */
+    private static void removeOldFiles() {
+        Collection<File> files = FileUtils.listFiles(new File("."), null, true);
+        
+        System.out.println("Removing old files...");
+        
+        files.forEach((file) -> {
+            String fileName = file.getPath().substring(1).replaceAll("\\\\", "/");
+        
+            if (canRemoveFile(file)) {
+                if (!NEW_FILES.contains(fileName)) {
+                    file.delete();
+                    System.out.println("Remove old file " + fileName);
+                }
+            }
+        });
+    }
+    
+    /**
      * Method that unpacks the download.
      * 
      * @param downloadFile Location of where the downloaded file is.
@@ -103,6 +188,9 @@ public final class PhantomUpdater {
             // Extract in the main folder to replace current files.
             fileName = fileName.substring(fileName.indexOf("/"));
             
+            // Save the file for later.
+            NEW_FILES.add(fileName);
+            
             // Print what we are extracting.
             System.out.println("Unpacking " + fileName + "...");
             
@@ -110,6 +198,9 @@ public final class PhantomUpdater {
             return (fileName.contains("config/audio-hooks") ||
                     fileName.contains("config/gif-alerts") ? null : fileName);
         });
+        
+        // Remove all old files.
+        removeOldFiles();
     }
     
     /**
@@ -120,7 +211,7 @@ public final class PhantomUpdater {
      * @return
      */
     private static boolean backupFiles(final String fileLocation, String backupLocation) {
-        if (fileLocation != null) {
+        if (fileLocation != null && backupLocation != null) {
             System.out.println("Creating a backup of PhantomBot...");
             
             // Replace all \ with /
